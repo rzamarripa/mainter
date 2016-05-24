af38c9513a050fa0f9e37144e9b5768485302d61
Jaime Ojeda <jaimeojeda@MacBook-Pro-de-Maria.local>
2016-05-23 19:16:25 -0600

    hola

--- client/listas/perfil.js
+++ client/listas/perfil.js
@@ -115,6 +115,50 @@ angular.module("interCeramic")
 		return departamento.nombre;
 	};
 
+
+	this.editarFondo = function(jefeArea)
+	{
+
+		var idTemp = jefeArea._id;
+		delete jefeArea._id;		
+		JefeAreas.update({_id:idTemp},{$set:jefeArea});
+		console.log(jefeArea);
+		document.getElementById("guardar").style.visibility = "hidden";
+		document.getElementById("input").style.visibility = "hidden";
+		document.getElementById("botonMostrar").style.visibility = "visible";
+
+	}
+	this.editarFondoGerente = function(gerente)
+	{
+
+		var idTemp = gerente._id;
+		delete gerente._id;		
+		Gerentes.update({_id:idTemp},{$set:gerente});
+		console.log(gerente);
+		document.getElementById("guardar").style.visibility = "hidden";
+		document.getElementById("input").style.visibility = "hidden";
+		document.getElementById("botonMostrar").style.visibility = "visible";
+
+	}
+	this.editarFondoEmpleado = function(empleado)
+	{
+
+		var idTemp = empleado._id;
+		delete empleado._id;		
+		Empleados.update({_id:idTemp},{$set:empleado});
+		console.log(empleado);
+		document.getElementById("guardar").style.visibility = "hidden";
+		document.getElementById("input").style.visibility = "hidden";
+		document.getElementById("botonMostrar").style.visibility = "visible";
+
+	}
+	this.mostrarBtnFondo = function(jefeArea,gerente,empleado)
+	{
+		document.getElementById("guardar").style.visibility = "visible";
+		document.getElementById("input").style.visibility = "visible";
+		document.getElementById("botonMostrar").style.visibility = "hidden";
+	}
+
 	/*rc.getOcupacion = function(id){
 		var ocupacion = Ocupaciones.findOne(rc.alumno.ocupacion_id);
 		return ocupacion.descripcion;
